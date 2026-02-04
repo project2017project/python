55009159431a837ca8ed139e599dedb595255105
NaveenKumar <project2017project@gmail.com>
2026-02-04 21:26:16 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+fruits = ['apple', 'banana', 'orange', 'grape']
+
+fruits.append("mango")
+
+fruits.insert(1,"pineapple")
+
+new_fruits = ['bluebarry','strawberry']
+
+fruits.extend((new_fruits))
+print(fruits)
